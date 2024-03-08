359ccd4dc0d9b17de6b6d43231cc3be28f78478f
Aniket Gupta <aniketgupta.org@gmail.com>
2024-03-08 06:59:54 +0530

    setup frontend

--- public/script.js
+++ public/script.js
@@ -1,6 +1,7 @@
 const shorturl = document.querySelectorAll(".shorturl");
 const copyBtn = document.querySelectorAll(".copybtn");
 const copyMsg = document.querySelector(".copiesmsg");
+const logoutBtn = document.querySelector("#logout");
 
 copyBtn.forEach((item, index) => item.addEventListener('click', () => {
     navigator.clipboard.writeText(shorturl[index].href);
@@ -9,3 +10,7 @@ copyBtn.forEach((item, index) => item.addEventListener('click', () => {
         copyMsg.style.display = "none";
     }, 4000);
 }));
+
+logoutBtn.addEventListener(()=>{
+    browser.cookies.remove("authId")
+})
