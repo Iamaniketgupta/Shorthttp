eccc9e770e6929bd33837a852a3f7e56f3635437
Aniket Gupta <aniketgupta.org@gmail.com>
2024-03-08 07:01:27 +0530

    chore: updated

--- index.js
+++ index.js
@@ -2,11 +2,13 @@ const express =require("express");
 const ejs =require("ejs");
 const path = require("path");
 const app =express();
+const cookieParser = require('cookie-parser')
 const router =require("./routes/routes");
 const dbConnect= require("./util/dbconfig");
 const getAllUrls = require("./controllers/getUrlController");
 const PORT =process.env.PORT || 8000;
 
+app.use(cookieParser());
 app.use(express.urlencoded({extended:true}));
 
 app.set("view engine",ejs);
@@ -18,6 +20,6 @@ app.set("views",path.resolve("./views"));
 
 app.use("/url",router); 
 
-app.get("/",getAllUrls);
+app.get("/",(req,res)=>res.render("home.ejs"));
 
 app.listen(PORT,()=>console.log("listening"));
