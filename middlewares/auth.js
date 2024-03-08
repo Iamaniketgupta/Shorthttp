c2180be34df8b8dc96abe1328d3798e58e2f721a
Aniket Gupta <aniketgupta.org@gmail.com>
2024-03-08 07:00:12 +0530

    feat: middleware for auth

---
+++ middlewares/auth.js
@@ -0,0 +1,24 @@
+const { getUser } = require("../util/auth");
+
+async function validateLoggedUser(req, res, next) {
+  const token = req.cookies?.authId;
+  if (!token) return res.redirect("/url/login");
+
+  const user = getUser(token);
+  if (!user) return res.redirect("/url/login");
+
+  req.user = user;  //sending req.user
+  next();
+}
+
+async function checkAuth(req, res, next) {
+  const token = req.cookies?.authId;
+  const user = getUser(token);  //util jwt verify
+  req.user = user;
+  next();
+}
+ 
+module.exports = {
+  validateLoggedUser,
+  checkAuth,
+};
